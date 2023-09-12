c6e933f014fd0736d7a0ab78b8787b2767e7e2a7
Alexey <ax.chinaev@yandex.ru>
2023-09-12 15:41:31 +0300

    add tables

--- internal/app/ds/tables.go
+++ internal/app/ds/tables.go
@@ -1,7 +1,37 @@
 package ds
 
-type Product struct {
-	ID    uint `gorm:"primarykey"`
-	Code  string
-	Price uint
+import "time"
+
+type Alpinist struct {
+	Id          uint `gorm:"primarykey"`
+	Name        string
+	Age         int
+	Country     string
+	ImageRef    string
+	Description string
+	Status      string
+}
+
+type User struct {
+	Id       uint `gorm:"primarykey"`
+	Login    string
+	Password string
+	ImageRef string
+}
+
+type Expedition struct {
+	Id        uint `gorm:"primarykey"`
+	Name      string
+	Year      int
+	Status    string
+	CreatedAt time.Time `json:"start_date"`
+	FormedAt  time.Time `json:"start_date"`
+	ClosedAt  time.Time `json:"start_date"`
+	moderator User
+}
+
+type AlpinistExpedition struct {
+	Id         uint `gorm:"primarykey"`
+	Alpinist   Alpinist
+	Expedition Expedition
 }
