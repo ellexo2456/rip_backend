225faccec66049ad06b4f73c1f27d27daaf68304
Alexey <ax.chinaev@yandex.ru>
2023-12-23 19:00:09 +0300

    ly

--- internal/app/ds/tables.go
+++ internal/app/ds/tables.go
@@ -12,9 +12,9 @@ type Expedition struct {
 	Name          string     `gorm:"type:varchar(90)" json:"name"`
 	Year          int        `json:"year"`
 	Status        string     `gorm:"type:varchar(90)" json:"status"`
-	CreatedAt     time.Time  `json:"-"`
-	FormedAt      time.Time  `json:"-"`
-	ClosedAt      time.Time  `json:"-"`
+	CreatedAt     time.Time  `json:"createdAt"`
+	FormedAt      time.Time  `json:"formedAt"`
+	ClosedAt      time.Time  `json:"closedAt"`
 	UserID        uint       `json:"-" gorm:"DEFAULT:NULL"`
 	Usr           *User      `gorm:"foreignkey:UserID" json:"user,omitempty"`
 	ModeratorUser *User      `gorm:"foreignkey:ModeratorID;" json:"moderator,omitempty"`
--- internal/app/dsn/dsn.go
+++ internal/app/dsn/dsn.go
@@ -6,15 +6,15 @@ import (
 )
 
 func FromEnv() string {
-	host := os.Getenv("POSTGRES_HOST")
+	host := os.Getenv("DB_HOST")
 	if host == "" {
 		return ""
 	}
 
-	port := os.Getenv("POSTGRES_PORT")
-	user := os.Getenv("POSTGRES_USER")
-	pass := os.Getenv("POSTGRES_PASSWORD")
-	dbname := os.Getenv("POSTGRES_DB")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	dbname := os.Getenv("DB_NAME")
 
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, pass, dbname)
 }
